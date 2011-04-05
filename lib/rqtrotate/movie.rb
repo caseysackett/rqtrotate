fee126ac8283208f6dbacaedb2c04b5b23e941b4
Chris Umbel <chris@chrisumbel.com>
2011-04-04 21:10:05 -0400

    commenting

--- lib/rqtrotate/movie.rb
+++ lib/rqtrotate/movie.rb
@@ -14,32 +14,42 @@
 # limitations under the License.
 
 module RQTRotate
+  # core object to managing movie rotation. represents a stream
+  # of quicktime data.
   class Movie
     attr_accessor :stream
     
+    # set (modify) the rotation in the structure matrix of the movie
     def rotation=(value)
       set_rotation @stream, value
       reset_stream
     end
     
+    # read the rotation value from the structure of the movie
     def rotation
       rotation = get_rotation @stream
       reset_stream
       rotation
     end
 
+    # open a movie so the caller can perform actions within the block,
+    # namely getting or setting the rotation. close when done.
     def self.open(file_name, &block)
       movie = Movie.new(File.open(file_name, File::RDWR), true)
       block.call movie      
       movie.stream.close
     end
     
+    # constructor. accept a stream of movie data and allow caller
+    # to specify ownership
     def initialize(stream, owns_stream = false)
       @stream = stream
       @owns_stream = owns_stream
     end
     
     private
+    # if being called in an open block we own the stream so we'll 
+    # reset it so future operations can start at the begining.
     def reset_stream
        @stream.seek(0, IO::SEEK_SET) if @owns_stream
     end
